174e36ef81ff1df17783b93537a94463e877b511
aniket_tarapurkar <aniket.v.tarapurkar99@gmail.com>
2023-04-30 15:05:39 +0530

    edited navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,16 +30,7 @@ const Header = () => {
               Add Product
             </Link>
           </li>
-          <li className="option" onClick={closeMobileMenu}>
-            <Link to="/check-out" className="item">
-              Cheack Out
-            </Link>
-          </li>
-          <li className="option" onClick={closeMobileMenu}>
-            <Link to="/aboute-project-and-me" className="item">
-              About
-            </Link>
-          </li>
+
           <li className="option" onClick={closeMobileMenu}>
             <Link to="/cart" className="item">
               <div className="ui ">
